45a6e1cc84737169b1a2fef9a9cf3170a41b02d4
Linay90 <linaybharambe1108@gmail.com>
2025-04-20 16:51:24 +0530

    list render

--- SplitBite/splitbite/src/App.js
+++ SplitBite/splitbite/src/App.js
@@ -27,16 +27,20 @@ function Button({children,onClick}){
  }
 
  export default function App(){
-  const[friends,setFriends]=useState([initialFriends])
+  const[friends,setFriends]=useState(initialFriends)
 
   const[showAddFriend,setShowAddFriend]= useState(false)
   function handleShowAddFriend(){
     setShowAddFriend((show)=>!show);
    }
+
+   function handleAddFriend(friend){
+    setFriends((friends)=>[...friends,friend])
+   }
   return<div className="app">
     <div className="sidebar">
-    <FriendList/>
-   {showAddFriend&&  <FormAddFriend/>}
+    <FriendList friends={friends}/>
+   {showAddFriend&&  <FormAddFriend onAddFriend={handleAddFriend}/>}
     <Button onClick={handleShowAddFriend}>{showAddFriend?"close":"Add friend"}</Button>
     </div>
     <FormSplitBill/>
@@ -44,8 +48,7 @@ function Button({children,onClick}){
   </div>
  }
 
- function FriendList(){
-  const friends=initialFriends;
+ function FriendList({friends}){
   return<ul>
     {friends.map((friend)=>(
       <Friend friend={friend} key={friend.id}/>
@@ -92,21 +95,24 @@ function Button({children,onClick}){
 
  
 
- function FormAddFriend(){
+ function FormAddFriend({onAddFriend}){
   const[name,setName]=useState("")
-  const[image,setImage]=useState("")
+  const[image,setImage]=useState("https://i.pravatar.cc/48")
 
   function handleSubmit(e){
     e.preventDefault();
     if(!name || !image) return
+    const id=crypto.randomUUID();
     const newFriend={
+      id,
       name,
-      image,
+      image:`${image}?=${id}`,
       balance:0,
-      id:crypto.randomUUID
+      
     };
+    onAddFriend(newFriend);
     setName("")
-    setImage("")
+    setImage("https://i.pravatar.cc/48")
 
   }
   return <form className="form-add-friend" onSubmit={handleSubmit}> 
